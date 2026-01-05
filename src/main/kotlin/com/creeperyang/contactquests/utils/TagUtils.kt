cd06fc11bf814e612c6afd0387fa961214d4c96c
NightFrost <creeperyang@outlook.com>
2026-01-05 19:23:59 +0800

    修复命名错误

--- src/main/kotlin/com/creeperyang/contactquests/utils/TagUtils.kt
+++ src/main/kotlin/com/creeperyang/contactquests/utils/TagUtils.kt
@@ -10,12 +10,10 @@ object TagUtils {
 
         file.allChapters.forEach { chapter ->
             chapter.quests.forEach { quest ->
-                // 1. 收集任务需求的 Tag (从 Mixin 接口)
                 if (quest is IQuestExtension) {
                     tags.addAll((quest as IQuestExtension).`contactQuests$getRequiredTags`())
                 }
 
-                // 2. 收集奖励提供的 Tag
                 quest.rewards.forEach { reward ->
                     if (reward is ParcelRewardBase) {
                         tags.addAll(reward.unlockTags)
